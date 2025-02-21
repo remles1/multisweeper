50cb43386bf0ce5166614d2e4ea6215e26da00ce
remles <115708921+remles1@users.noreply.github.com>
2025-02-21 19:10:31 +0100

    setting up

--- multisweeper/views.py
+++ multisweeper/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+
+
+
 
 def index(request):
     return render(request, "multisweeper/index.html")
+
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('multisweeper.urls')),
     path('admin/', admin.site.urls),
+    path("accounts/", include("django_registration.backends.one_step.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 
 ]
