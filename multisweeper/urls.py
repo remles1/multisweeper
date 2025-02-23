896f01e9607d86368c46c6d9af492b37154484c9
remles <115708921+remles1@users.noreply.github.com>
2025-02-23 22:38:37 +0100

    beggining of the guest feature

--- multisweeper/urls.py
+++ multisweeper/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("create-lobby/", views.create_lobby, name="create_lobby"),
     path("lobby/<str:lobby_id>/", views.lobby, name="lobby"),
+    path("pick-username/", views.pick_username, name="pick_username")
 ]
--- multisweeper/views.py
+++ multisweeper/views.py
@@ -24,4 +24,24 @@ def create_lobby(request):
 def lobby(request, lobby_id):
     if lobby_id not in lobbies:
         raise Http404("No such lobby")
+
+    if not lobbies[lobby_id].current_players < lobbies[lobby_id].max_players:
+        raise Http404("lobby full")
+
+    if not request.user.is_authenticated:
+        if 'username' not in request.session:
+            return redirect(f'/pick-username/?next=/lobby/{lobby_id}/')
+
     return render(request, "multisweeper/lobby.html", {"lobby_id": lobby_id, "mine_count": lobbies[lobby_id].game_instance.mine_count})
+
+
+def pick_username(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+
+        request.session['username'] = username
+
+        next_url = request.GET.get('next', '/')
+        return redirect(next_url)
+
+    return render(request, 'multisweeper/pick_username.html')
