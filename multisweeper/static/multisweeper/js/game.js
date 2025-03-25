87a2074840f724a6f9aebac5124419a6cce72dbe
remles <115708921+remles1@users.noreply.github.com>
2025-03-25 19:48:57 +0100

    bomb functionality frontend

--- multisweeper/static/multisweeper/js/game.js
+++ multisweeper/static/multisweeper/js/game.js
@@ -8,37 +8,47 @@ mines_left_update_counter(minesLeft);
 update_seconds_counter(0)
 let username = null;
 let state = ""
+let bombModeActivated = false;
+let width = 0;
+let height = 0;
+
+// document.querySelector('#new-game-button').addEventListener('click', () => {
+//     restart_game();
+//     const stats_div = document.getElementById("stats");
+//     stats_div.style.display = "none";
+//
+// });
 
 
+// function restart_game(){
+//     const message = JSON.stringify({
+//         type: "new_game",
+//         message: ""
+//     })
+//     socket.send(message)
+//     firstClick = true;
+//     startTime = null;
+//     timerRunning = false;
+//     minesLeft = mine_count;
+//     mines_left_update_counter(minesLeft);
+//
+//     if (timerInterval) {
+//         clearInterval(timerInterval);
+//     }
+//     timerInterval = null;
+//     update_seconds_counter(0);
+//     document.querySelector('#new-game-button').classList.value = 'face-button face-neutral';
+// }
 
-document.querySelector('#new-game-button').addEventListener('click', () => {
-    restart_game();
-    const stats_div = document.getElementById("stats");
-    stats_div.style.display = "none";
-
-});
-
-function restart_game(){
-    const message = JSON.stringify({
-        type: "new_game",
-        message: ""
-    })
-    socket.send(message)
-    firstClick = true;
-    startTime = null;
-    timerRunning = false;
-    minesLeft = mine_count;
-    mines_left_update_counter(minesLeft);
-
-    if (timerInterval) {
-        clearInterval(timerInterval);
+document.querySelector('#bomb').addEventListener('click', () => {
+    const bomb = document.querySelector('#bomb');
+    if (bombModeActivated) {
+        bomb.className = 'bomb bomb-off';
+    } else {
+        bomb.className = 'bomb bomb-on';
     }
-    timerInterval = null;
-    update_seconds_counter(0);
-    document.querySelector('#new-game-button').classList.value = 'face-button face-neutral';
-}
-
-
+    bombModeActivated = !bombModeActivated
+})
 
 
 let user_board_dict = {
@@ -68,32 +78,25 @@ socket.onopen = function (e) {
 
 socket.onmessage = function (e) {
     const data = JSON.parse(e.data);
-    if(data["type"] === "seats"){
+    if (data["type"] === "seats") {
         render_seats_and_controls(data);
-    }
-    else if(data["type"] === "chat_message"){
+    } else if (data["type"] === "chat_message") {
         display_message_from_server(data["message"]);
-    }
-    else if(data["type"] === "game_over"){
-        if (data["draw"]){
+    } else if (data["type"] === "game_over") {
+        if (data["draw"]) {
             alert('draw');
-        }
-        else{
+        } else {
             let winner_username = data["winner_username"]
             alert(`${winner_username} is the winner! Congratulations!`);
         }
 
-    }
-    else if(data["type"] === "state"){
+    } else if (data["type"] === "state") {
         state = data["message"];
-    }
-    else if(data["type"] === "username"){
+    } else if (data["type"] === "username") {
         username = data["message"];
-    }
-    else if(data["type"] === "game_in_progress_redirect"){
+    } else if (data["type"] === "game_in_progress_redirect") {
         location.replace("/game-in-progress/");
-    }
-    else if(data["type"] === "user_board"){
+    } else if (data["type"] === "user_board") {
         let user_board = JSON.parse(data["message"])
         let user_board_mapped = user_board.map(row =>
             row.map(cell => user_board_dict[cell])
@@ -111,12 +114,11 @@ socket.onmessage = function (e) {
                 div.replaceChild(clonedChild, child);
             });
 
-            if(data["won"]){
+            if (data["won"]) {
                 mines_left_update_counter(0);
-                update_seconds_counter(Math.floor(data["time"]/1000))
+                update_seconds_counter(Math.floor(data["time"] / 1000))
                 document.querySelector('#new-game-button').classList.value = 'face-button face-happy';
-            }
-            else {
+            } else {
                 document.querySelector('#new-game-button').classList.value = 'face-button face-sad';
             }
             let timeSpent = data["time"] / 1000;
@@ -134,12 +136,15 @@ function render_user_board(user_board, user_board_mapped) {
     board.innerHTML = '';
 
     minesLeft = mine_count;
+    height = user_board.length;
+    width = user_board[0].length;
+
     for (let i = 0; i < user_board.length; i++) {
         for (let j = 0; j < user_board[0].length; j++) {
             let cell = document.createElement("div");
             cell.id = "id" + i + "-" + j;
             cell.className = "cell " + user_board_mapped[i][j];
-            if(user_board[i][j].includes("f")){
+            if (user_board[i][j].includes("f")) {
                 minesLeft--;
             }
             cell.addEventListener("mousedown", cellMouseDown);
@@ -153,21 +158,20 @@ function render_user_board(user_board, user_board_mapped) {
     mines_left_update_counter(minesLeft)
 }
 
-function render_seats_and_controls(data){
+function render_seats_and_controls(data) {
     const seats = data["seats"];
     const scores = data["scores"];
     const elo_ratings = data["elo_ratings"];
     const active_seat = `${data["active_seat"]}`;
     const owner = `${data["owner"]}`;
     const start_game_button = document.getElementById("start-game-button");
-    if(username === owner && state !== 'LobbyGameInProgressState'){
+    if (username === owner && state !== 'LobbyGameInProgressState') {
         start_game_button.style.display = 'inline';
-    }
-    else {
+    } else {
         start_game_button.style.display = 'none';
     }
 
-    for(let key in seats){
+    for (let key in seats) {
         const score_div = document.getElementById(`score-${key}`);
         const player_color_icon = document.getElementById(`player-color-icon-${key}`);
         const player_seat_span = document.getElementById(`player-seat-${key}`);
@@ -176,34 +180,31 @@ function render_seats_and_controls(data){
         const elo_div = document.getElementById(`elo-${key}`)
 
         // player colors and turn indicator
-        if(active_seat === key){
+        if (active_seat === key) {
             player_color_icon.className = `triangle triangle-${key}`;
-        }
-        else {
+        } else {
             player_color_icon.className = `circle circle-${key}`;
         }
 
 
         // if the username is the owner (username is a global variable assigned to the file),
         // show owner controls along every seat
-        if(username === owner && state !== 'LobbyGameInProgressState'){
+        if (username === owner && state !== 'LobbyGameInProgressState') {
             owner_player_controls.style.display = 'inline';
-        }
-        else {
+        } else {
             owner_player_controls.style.display = 'none';
         }
 
 
-        if(username_in_this_seat === 'null'){
-            player_seat_span.innerHTML = `Empty seat #${parseInt(key)+1}`;
+        if (username_in_this_seat === 'null') {
+            player_seat_span.innerHTML = `Empty seat #${parseInt(key) + 1}`;
 
             // if the seat is empty, hide the owner controls
             owner_player_controls.style.display = 'none';
-        }
-        else{
+        } else {
             player_seat_span.innerHTML = `${username_in_this_seat}`;
         }
-        if(username_in_this_seat === owner){
+        if (username_in_this_seat === owner) {
 
 
             player_seat_span.innerHTML = `${username_in_this_seat} OWNER`;
@@ -218,14 +219,12 @@ function render_seats_and_controls(data){
             score_div.innerHTML = `SCORE: ${scores[key]}`;
         }
 
-        if (elo_ratings[key] === null){
+        if (elo_ratings[key] === null) {
             elo_div.innerHTML = '-';
-        }
-        else {
-            if (elo_ratings[key] === '--GUEST--'){
+        } else {
+            if (elo_ratings[key] === '--GUEST--') {
                 elo_div.innerHTML = `${elo_ratings[key]}`
-            }
-            else {
+            } else {
                 elo_div.innerHTML = `${elo_ratings[key]} ELO`;
             }
 
@@ -249,16 +248,60 @@ function cellMouseDown(event) {
 
 function cellMouseOver(event) {
     event.preventDefault();
-    if (leftPressed) {
+    if (leftPressed && !bombModeActivated) {
         this.classList.add(user_board_dict["pressed"]);
+    }
+    else if(bombModeActivated){  // bomb mode highlighting
+        const numbers = this.id.match(/\d+/g).map(Number);
+        let cy = numbers[0];
+        let cx = numbers[1];
+
+        console.log(user_board_dict["pressed"]);
+
+        for (let dcy = cy-2; dcy <= cy + 2; dcy++){
+            for (let dcx = cx-2; dcx <= cx + 2; dcx++){
+                if ((dcy < 0 || dcy >= width) || (dcx < 0 || dcx >= height)){
+                    continue;
+                }
+
+                let cell = document.getElementById(`id${dcy}-${dcx}`);
+
+                if (cell.classList.contains(user_board_dict["c"])){
+                    cell.classList.remove(user_board_dict["c"]);
+                    cell.classList.add(user_board_dict["pressed"]);
+                }
+
+            }
+        }
     }
 }
 
 function cellMouseLeave(event) {
     event.preventDefault();
-    if (this.classList.contains(user_board_dict["pressed"])) {
-        this.classList.remove(user_board_dict["pressed"]);
+    if(!bombModeActivated){
+        if (this.classList.contains(user_board_dict["pressed"])) {
+            this.classList.remove(user_board_dict["pressed"]);
+        }
     }
+
+    else { //clean up after bomb mode highlighting
+        const numbers = this.id.match(/\d+/g).map(Number);
+        let cy = numbers[0];
+        let cx = numbers[1];
+        for (let dcy = cy-2; dcy <= cy + 2; dcy++){
+                for (let dcx = cx-2; dcx <= cx + 2; dcx++){
+                    if ((dcy < 0 || dcy >= width) || (dcx < 0 || dcx >= height)){
+                        continue;
+                    }
+                    let cell = document.getElementById(`id${dcy}-${dcx}`);
+                    if (cell.classList.contains(user_board_dict["pressed"])) {
+                        cell.classList.remove(user_board_dict["pressed"]);
+                        cell.classList.add(user_board_dict["c"]);
+                    }
+                }
+        }
+    }
+
 }
 
 function cellMouseUp(event) {
@@ -269,10 +312,7 @@ function cellMouseUp(event) {
 
     if (this.classList.contains(user_board_dict["0"])) {
         return;
-    }
-
-
-    else {
+    } else {
         const message = JSON.stringify({
             type: "l_click",
             message: this.id.replace(/^id/, "")
@@ -289,7 +329,7 @@ function cellMouseUp(event) {
     }
 }
 
-function chooseSeat(seat){
+function chooseSeat(seat) {
     const message = JSON.stringify({
         type: "choose_seat",
         message: seat,
@@ -297,7 +337,7 @@ function chooseSeat(seat){
     socket.send(message)
 }
 
-function promoteToOwner(seat){
+function promoteToOwner(seat) {
     const message = JSON.stringify({
         type: "promote_to_owner",
         message: seat,
@@ -305,7 +345,7 @@ function promoteToOwner(seat){
     socket.send(message)
 }
 
-function startGame(){
+function startGame() {
     const message = JSON.stringify({
         type: "start_game",
     })
@@ -315,36 +355,36 @@ function startGame(){
 function timer() {
     clearInterval(timerInterval)
     let seconds = 0;
-    timerInterval=setInterval(()=>{
+    timerInterval = setInterval(() => {
         if (!timerRunning) {
             return;
         }
         seconds += 1;
         update_seconds_counter(seconds)
-    },1000);
+    }, 1000);
 }
 
-function update_seconds_counter(seconds){
+function update_seconds_counter(seconds) {
     let seconds_as_digits = convertNumberTo3Digits(seconds);
-        for(let i = 0; i < 3; i++){
-            let digit_div = document.getElementById(`sc_d${i}`)
-            digit_div.classList.value = `digit digit-${seconds_as_digits[i]}`;
-        }
+    for (let i = 0; i < 3; i++) {
+        let digit_div = document.getElementById(`sc_d${i}`)
+        digit_div.classList.value = `digit digit-${seconds_as_digits[i]}`;
+    }
 }
 
-function mines_left_update_counter(mine_count){
+function mines_left_update_counter(mine_count) {
     let mines_left_as_digits = convertNumberTo3Digits(mine_count);
-    for(let i = 0; i < 3; i++){
-            let digit_div = document.getElementById(`mlc_d${i}`)
-            digit_div.classList.value = `digit digit-${mines_left_as_digits[i]}`;
+    for (let i = 0; i < 3; i++) {
+        let digit_div = document.getElementById(`mlc_d${i}`)
+        digit_div.classList.value = `digit digit-${mines_left_as_digits[i]}`;
     }
 }
 
-function convertNumberTo3Digits(number){
-    if (number > 999){
-        return ['9','9','9'];
+function convertNumberTo3Digits(number) {
+    if (number > 999) {
+        return ['9', '9', '9'];
     }
-    let formatted = number.toString().padStart(3,'0');
+    let formatted = number.toString().padStart(3, '0');
     return formatted.split('');
 }
 
@@ -352,20 +392,20 @@ function convertNumberTo3Digits(number){
 * CHAT FUNCTIONS
 */
 
-function hackyEscaper(s){
+function hackyEscaper(s) {
     // not really needed tbh
     let div = document.createElement("div");
     div.textContent = s;
     return div.innerHTML;
 }
 
-function display_message_from_server(message){
+function display_message_from_server(message) {
     const chatBox = document.getElementById("chat-box");
     const messageElem = document.createElement("div");
     messageElem.classList.add("message");
     chatBox.appendChild(messageElem);
 
-    if(message.startsWith("SERVER:")){
+    if (message.startsWith("SERVER:")) {
         messageElem.style.background = "#4CAF50";
     }
 
@@ -382,13 +422,12 @@ function sendMessageLocalPart() {
 
     const chatBox = document.getElementById("chat-box");
 
-    if (message.length > 140){
+    if (message.length > 140) {
         const messageElem = document.createElement("div");
         messageElem.classList.add("message");
         chatBox.appendChild(messageElem);
         messageElem.textContent = '--- MESSAGE TOO LONG ---';
-    }
-    else {
+    } else {
         message = hackyEscaper(message);
         //messageElem.textContent = message; //it escapes it one more time for good measure why not
         sendMessageToServer(message);
@@ -397,7 +436,7 @@ function sendMessageLocalPart() {
     input.value = "";
 }
 
-function sendMessageToServer(message){
+function sendMessageToServer(message) {
 
     const chat_message_json = JSON.stringify({
         type: "chat_message",
@@ -426,3 +465,7 @@ setChatBoxMaxHeight();
 /*
 * END CHAT FUNCTIONS
 */
+
+function activateBombMode() {
+
+}
